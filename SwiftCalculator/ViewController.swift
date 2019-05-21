0794b5a6a57fdce497c24f432b4d4a4062a7face
Abraham Vazquez <mvazquezp1000@alumno.ipn.mx>
2019-05-21 11:37:29 -0500

    La calculadora se termina, pero queda una mejora que hacer, falta pintar el total de la propina generada

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -9,12 +9,96 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - O U T L E T S
+    @IBOutlet weak var txfAccount: UITextField!
+    @IBOutlet weak var txfTip:     UITextField!
+    @IBOutlet weak var txtTotal:   UITextField!
+    @IBOutlet weak var btnTip5:    UIButton!
+    @IBOutlet weak var btnTip10:   UIButton!
+    @IBOutlet weak var btnTip15:   UIButton!
+    @IBOutlet weak var btnTip20:   UIButton!
+    
+    //MARK: - V A R I A B L E S
+    var fAmount:  Float  = 0.0
+    var fTip:     Float  = 0.0
+    var fTotal:   Float  = 0.0
+    var percent:  String = ""
+    var strTip:   String = ""
+    
+    //MARK: - L I F E · C Y C L E
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - F U N C T I O N S
+    func getAmount(strAccount: String) -> Float
+    {
+        fAmount = Float(strAccount)!
+        return fAmount
+    }
+    
+    // Devuelve el Tip para multiplicarlo
+    func convertTipToOperate(percent: String) -> Float
+    {
+        let strPoint: String = "0."
+        let strTipConverted: String = strPoint + percent
+        let fTipToOperatre: Float = Float(strTipConverted)!
+        txfTip.text = String(fTipToOperatre)
+        return fTipToOperatre
+    }
 
-
+    func realizaOperacionDelPorcentaje(fAmount: Float, fTipConvertido: Float) -> Float {
+        
+        return fAmount * fTipConvertido
+        
+    }
+    
+    func sumaDeResultados(fAmount: Float, fTip: Float) -> Float{
+        
+        return fAmount + fTip
+    }
+   
+    func printTotal()
+    {
+        txtTotal.text = String(self.sumaDeResultados(fAmount: self.getAmount(strAccount: txfAccount.text!), fTip: self.realizaOperacionDelPorcentaje(fAmount: self.getAmount(strAccount: txfAccount.text!), fTipConvertido: self.convertTipToOperate(percent: percent))))
+    }
+    
+    //MARK: - V A L I D A T I O N S
+    func bExistTip(srtTip:String) -> Bool {
+        if txfTip.text == ""{
+            return false
+        }else { return true }
+    }
+    
+//    func printAmount(WithAmount strAmount: String, andTip strTip: String) {
+//        if strTip.isEmpty {
+//            txtTotal.text = strAmount
+//        }else{
+//            total.text = "" // CANTIDAD SUMADA
+//        }
+//    }
+    
+    //MARK: - A C T I O N S
+    @IBAction func btn5Percent(_ sender: Any) {
+        percent = "05"
+        self.printTotal()
+    }
+    
+    @IBAction func btn10Percent(_ sender: Any) {
+        percent = "10"
+        self.printTotal()
+    }
+    
+    @IBAction func btn15Percent(_ sender: Any) {
+        percent = "15"
+        self.printTotal()
+    }
+    
+    @IBAction func btn20Percent(_ sender: Any) {
+        percent = "20"
+        self.printTotal()
+        
+    }
 }
-
